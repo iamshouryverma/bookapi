7dcf7c04e8f6cebf18ec81ce4eca7ee106e95556
Shoury <iamsv143@gmail.com>
2024-01-05 14:13:09 +0530

    Initial commit

---
+++ Apiproject/ApiApplication/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Book(models.Model):
+    id=models.IntegerField(primary_key=True)
+    title=models.CharField(max_length=200)
+    author=models.CharField(max_length=200)    
